c0539e14d0b6454f350973694a9ba9da012d7bfb
Muhammad Rafi Kurniawa <muhammadrafi9b24@gmail.com>
2024-11-18 23:27:01 +0700

    image picker

--- screens/FoundItemForm.js
+++ screens/FoundItemForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 import tw from 'twrnc';
 
 export default function LostItemForm({ onClose }) {
@@ -7,13 +8,33 @@ export default function LostItemForm({ onClose }) {
   const [itemDescription, setItemDescription] = useState('');
   const [locationFound, setLocationFound] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [selectedImage, setSelectedImage] = useState(null); // State untuk gambar
 
   const handleSubmit = () => {
     if (!itemName || !itemDescription || !locationFound || !phoneNumber) {
       alert('ISI DULU KOCAG!!!');
     } else {
-      console.log({ itemName, itemDescription, locationFound, phoneNumber });
-      onClose(); 
+      console.log({ itemName, itemDescription, locationFound, phoneNumber, selectedImage });
+      onClose();
+    }
+  };
+
+  const pickImage = async () => {
+    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert('Izin untuk mengakses galeri diperlukan!');
+      return;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
     }
   };
 
@@ -46,13 +67,23 @@ export default function LostItemForm({ onClose }) {
         onChangeText={setPhoneNumber}
       />
 
-      <TouchableOpacity style={tw`bg-gray-300 w-2/5 h-8 rounded-xl flex justify-center items-center mx-auto mb-5`}>
+      <TouchableOpacity
+        style={tw`bg-gray-300 w-2/5 h-8 rounded-xl flex justify-center items-center mx-auto mb-5`}
+        onPress={pickImage}
+      >
         <Text style={tw`text-white font-bold`}>Upload File</Text>
       </TouchableOpacity>
 
+      {selectedImage && (
+        <Image
+          source={{ uri: selectedImage }}
+          style={tw`w-20 h-20 rounded-xl mx-auto`}
+        />
+      )}
+
       <TouchableOpacity
         style={tw`bg-green-500 p-3 rounded-xl flex justify-center items-center`}
-        onPress={handleSubmit} 
+        onPress={handleSubmit}
       >
         <Text style={tw`text-white font-bold`}>SUBMIT</Text>
       </TouchableOpacity>
